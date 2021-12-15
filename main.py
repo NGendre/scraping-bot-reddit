0eaffc63d2fd89b1c35b6cc28acc3f2273ac7fe1
NGendre <noe.gendre@outlook.fr>
2021-12-15 14:56:53 +0100

    -Fixed an issue when no next pages were available -Added csv output

--- main.py
+++ main.py
@@ -2,22 +2,46 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+import csv
+import os
 
 
 #REDDIT
 
-#Récpère la page
+#initialisation
+url = input("Please enter an URL: ")
+
+
+
 browser = webdriver.Chrome()
-browser.get("https://old.reddit.com/r/globaloffensive")
+
+browser.get(url)
 titles = []
 
+nextPage = 0
 
-posts = browser.find_elements(By.CLASS_NAME,("thing"))
-for p in posts:
-    if "promoted" not in p.get_attribute("class"):
-        title = p.find_element(By.CLASS_NAME,"top-matter")
-        title = title.find_element(By.CLASS_NAME,"may-blank")
-        titles.append(title.text)
+while nextPage<5:
+    sleep(0.5)
+    currentPage = browser.current_url
+    browser.get(currentPage)
+    try:
+        posts = browser.find_elements(By.CLASS_NAME,("thing"))
+        for p in posts:
+            #Filters ads posts
+            if "promoted" not in p.get_attribute("class"):
+                title = p.find_element(By.CLASS_NAME,"top-matter")
+                title = title.find_element(By.CLASS_NAME,"may-blank")
+                titles.append(title.text)
+        nextButton = browser.find_element(By.CLASS_NAME,("next-button"))
+        nextPage += 1
+        nextButton.click()
+    except:
+        break
 
-print(titles)
+
+location = os.path.join(os.path.dirname(__file__),'results.csv')
+with open(location,'w',newline='',encoding="utf-8") as file:
+    wr = csv.writer(file)
+    for t in titles:
+        wr.writerow(t)
+    file.close()
